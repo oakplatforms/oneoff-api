eea48322921ba75d0a284be240892f6490e8d60e
Skylar Banks <skylarcbanks@gmail.com>
2025-02-25 05:21:52 -0500

    add jest and supertest framework to oak api

---
+++ jest.setup.ts
@@ -0,0 +1,15 @@
+const { PrismaClient } = require('@prisma/client')
+const { execSync } = require('child_process')
+
+const prisma = new PrismaClient()
+
+beforeAll(async () => {
+  execSync('npx prisma migrate reset --force', { stdio: 'inherit' })
+  execSync('npx prisma migrate dev', { stdio: 'inherit' });
+});
+
+afterAll(async () => {
+  await prisma.$disconnect()
+});
+
+export { prisma }
