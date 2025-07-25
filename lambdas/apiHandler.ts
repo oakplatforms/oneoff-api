e314c1a67c9fcb8d276141e7167ec06158dcf4c8
Skylar Banks <skylarcbanks@gmail.com>
2025-07-25 06:01:11 -0400

    does this work?

--- lambdas/apiHandler.ts
+++ lambdas/apiHandler.ts
@@ -1,4 +1,17 @@
 import { default as serverlessExpress } from '@vendia/serverless-express'
 import { app } from '../src/index'
 
-export const handler = serverlessExpress({ app })
+const serverlessHandler = serverlessExpress({ app })
+
+export const handler = (event, context) => {
+  //For HTTP API v2, manually extract authorizer data and add it to headers
+  if (event.requestContext?.authorizer?.lambda) {
+    const authorizerData = event.requestContext.authorizer.lambda
+    if (!event.headers) event.headers = {}
+    event.headers['x-authorizer-role'] = authorizerData.role
+    event.headers['x-authorizer-userpool'] = authorizerData.userPool
+    event.headers['x-authorizer-principalid'] = authorizerData.principalId || 'unknown'
+  }
+
+  return serverlessHandler(event, context, () => {})
+}
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,6 @@
 import express, { Request, Response } from 'express'
 import router from './routers/all_routes'
 
-interface ExtendedRequest extends Request {
-  apiGateway?: {
-    event: {
-      requestContext?: {
-        authorizer?: {
-          lambda?: {
-            principalId?: string
-            role: string
-            userPool: string
-          }
-        }
-      }
-    }
-  }
-  event?: unknown
-  lambdaEvent?: unknown
-}
-
 declare module 'express-serve-static-core' {
   interface Request {
     user?: {
@@ -40,28 +22,25 @@ app.use(express.json({ limit: '10mb' }))
 app.use((req, res, next) => {
   console.log('🔍 MIDDLEWARE CALLED for:', req.method, req.path)
 
-  //Debug what's available on the request object
-  console.log('Full req keys:', Object.keys(req))
-  console.log('Request apiGateway:', (req as ExtendedRequest).apiGateway)
-  console.log('Request event:', (req as ExtendedRequest).event)
-  console.log('Request lambdaEvent:', (req as ExtendedRequest).lambdaEvent)
+  //Read authorizer data from custom headers (set by Lambda handler)
+  const headerRole = req.headers['x-authorizer-role'] as string
+  const headerUserPool = req.headers['x-authorizer-userpool'] as string
+  const headerPrincipalId = req.headers['x-authorizer-principalid'] as string
 
-  const rawEvent = (req as ExtendedRequest).apiGateway?.event
-  const lambdaContext = rawEvent?.requestContext?.authorizer?.lambda
+  console.log('Header Role:', headerRole)
+  console.log('Header UserPool:', headerUserPool)
+  console.log('Header PrincipalId:', headerPrincipalId)
 
-  console.log('Raw event:', rawEvent ? 'Found' : 'Not found')
-  console.log('Lambda context:', lambdaContext)
-
-  if (!lambdaContext) {
-    console.log('No authorizer context found - req.user will be undefined')
-  } else {
-    console.log('Found authorizer context:', lambdaContext)
+  if (headerRole && headerUserPool) {
+    console.log('Found authorizer data in headers')
     req.user = {
-      principalId: lambdaContext.principalId || 'unknown',
-      role: lambdaContext.role,
-      userPool: lambdaContext.userPool
+      principalId: headerPrincipalId || 'unknown',
+      role: headerRole,
+      userPool: headerUserPool
     }
-    console.log('Set req.user:', req.user)
+    console.log('Set req.user from headers:', req.user)
+  } else {
+    console.log('No authorizer data found in headers - req.user will be undefined')
   }
 
   next()
