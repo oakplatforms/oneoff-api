4ed322a064c49db026cda61235cef5c48765cccf
Skylar Banks <skylarcbanks@gmail.com>
2025-07-25 05:16:29 -0400

    test top level lambda handler

--- lambdas/apiHandler.ts
+++ lambdas/apiHandler.ts
@@ -1,4 +1,16 @@
 import { default as serverlessExpress } from '@vendia/serverless-express'
 import { app } from '../src/index'
 
-export const handler = serverlessExpress({ app })
+const serverlessHandler = serverlessExpress({ app })
+
+export const handler = (event, context) => {
+  console.log('🔍 LAMBDA EVENT:', JSON.stringify(event, null, 2))
+  console.log('🔍 LAMBDA CONTEXT:', JSON.stringify(context, null, 2))
+
+  //For HTTP API v2, manually extract authorizer data and add it to the event
+  if (event.requestContext?.authorizer) {
+    console.log('🔍 AUTHORIZER FOUND IN EVENT:', JSON.stringify(event.requestContext.authorizer, null, 2))
+  }
+
+  return serverlessHandler(event, context, () => {})
+}
