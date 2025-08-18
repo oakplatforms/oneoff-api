035ccfd8a285078881b6256a57c5045c7eacf5fe
Skylar Banks <skylarcbanks@gmail.com>
2025-08-18 00:12:24 -0400

    fix issue with bad logs

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -139,7 +139,7 @@ userRouter.post(`/user`, async (req, res) => {
   const { profile: profileProps, carts: cartsProps, ...accountProps } = account || {}
 
   try {
-    console.log('req.user', req.user)
+    console.log('admin user', req.user, isAdmin, admin)
     validateRole(req.user as AuthenticatedUser, 'admin')
     const user = await prisma.user.create({
       data: {
