159e66dd4f1bef815ca481cd04f8de5af5390ecb
Skylar Banks <skylarcbanks@gmail.com>
2025-08-26 20:46:00 -0400

    revert architecture change

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express'
+import cors from 'cors'
 import router from './routers/all_routes'
 
 declare module 'express-serve-static-core' {
@@ -16,6 +17,14 @@ process.on('unhandledRejection', (reason) => {
 })
 
 const app = express()
+
+// CORS middleware
+app.use(cors({
+  origin: true, // Allow all origins in development
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-authorizer-role', 'x-authorizer-userpool', 'x-authorizer-principalid']
+}))
 
 app.use(express.json({ limit: '10mb' }))
 
