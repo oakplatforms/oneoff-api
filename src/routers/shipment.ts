391a18b55bb61bbbed5897e6fae3fd80c34a0826
Skylar Banks <skylarcbanks@gmail.com>
2025-12-11 20:47:26 -0500

    cheapestOutboundRate log

--- src/routers/shipment.ts
+++ src/routers/shipment.ts
@@ -336,7 +336,7 @@ shipmentRouter.post('/tracked-shipment', async (req, res) => {
 
       const cheapestOutbound = sortedOutboundRatesWithShipment[0] || null
       const cheapestOutboundRate = cheapestOutbound?.rate || null
-
+      console.log('cheapestOutboundRate', JSON.stringify(cheapestOutboundRate, null, 2))
       //Create OUTBOUND shipment record
       const outboundShipment = await tx.shipment.create({
         data: {
